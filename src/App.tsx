287e3bbbc2423cc9f24dc13cf5d7c7773568b970
kybaq <flfsk12@gmail.com>
2025-03-14 12:17:24 +0900

    feat: AuthPage 컴포넌트 추가 및 기본 스타일 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import appLogo from '/favicon.svg';
 import PWABadge from './PWABadge.tsx';
 import './App.css';
-import { Button } from './components/ui/button.tsx';
+import AuthPage from './Routers/auth/AuthPage.tsx';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={appLogo} className="logo" alt="Rhythmix logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-[320px] h-[598px] border mx-auto flex flex-col justify-center text-center">
+        <PWABadge />
+        <AuthPage />
       </div>
-      <h1>Rhythmix</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test <span className="underline">sdf</span>HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-      <PWABadge />
-      <Button>shadcn 버튼 입니다</Button>
     </>
   );
 }

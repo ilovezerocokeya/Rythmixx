9f1befc9df22922578382020dbabb7dd9c5c0d10
kybaq <flfsk12@gmail.com>
2025-03-14 12:18:36 +0900

    feat: 회원가입 컴포넌트 라우터 컴포넌트 생성

---
+++ src/Routers/auth/AuthPage.tsx
@@ -0,0 +1,12 @@
+import AuthForm from '@/components/auth/AuthForm';
+
+const AuthPage = () => {
+  return (
+    <>
+      <p>Rhythmix 에서 나에게 딱 맞는 노래를 모아 Mix 하세요!</p>
+      <AuthForm />
+    </>
+  );
+};
+
+export default AuthPage;

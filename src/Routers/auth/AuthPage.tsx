c02304ef181b1ac4b166fa96832bb1c35e45e5f2
kybaq <flfsk12@gmail.com>
2025-03-19 15:34:24 +0900

    feat: 로그인 페이지 추가

--- src/Routers/auth/AuthPage.tsx
+++ src/Routers/auth/AuthPage.tsx
@@ -3,7 +3,7 @@ import AuthForm from '@/components/auth/AuthForm';
 const AuthPage = () => {
   return (
     <>
-      <p>Rhythmix 에서 나에게 딱 맞는 노래를 모아 Mix 하세요!</p>
+      <p className="text-white">Rhythmix 에서 나에게 딱 맞는 노래를 모아 Mix 하세요!</p>
       <AuthForm />
     </>
   );
